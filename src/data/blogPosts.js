0971d4bc19e8871bee7c908bd97a9156e119111b
maanasgarimella <garimellamaanas@gmail.com>
2025-09-30 15:03:42 -0500

    Added Post Summer

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -3,7 +3,7 @@
 export const blogPosts = [
   {
     slug: 'i-graduated',
-    title: 'I Graduated.',
+    title: 'i graduated',
     date: '2025-06-01',
     content: `
 I've always maintained that graduation isn't a happy moment, but a sad one; but now I'm inclined to change my mind.  
@@ -14,16 +14,41 @@ I've never been the type to dance to parties or get excited when some obscure ra
 ,
   },
   {
-    slug: 'upcoming-posts',
-    title: 'Upcoming Blog Posts!',
+    slug: 'summer-thoughts',
+    title: 'summer thoughts',
     date: '2025-06-08',
     content: `
-Sneak peek at what's coming next in Neuro 101 and more.
+I’ve found that there are two types of pain.
+
+Physical, and Emotional.
+
+Obviously, these can be further divided – nostalgia, burns, scratches, numbness – but, these are the two that loom large.
+
+At the end of the day, they’re both pain – they are both distressful and bring tears to our eyes, and a desire to never feel such a thing again. But recently, that hasn’t been the case.
+
+While physical pain still brings me the same suffering, I’ve found myself leaning just a little bit more into the one that comes and remains purely within my mind. 
+
+After all, depression is human kind’s magnum opus.
+
+When a family member dies, we mourn. It has no logical basis – in fact the opposite. Animals tend to stay away from the dead; wary of the invisible parasites that could lie within their bodies. The only ‘logical’ reason to stay with the dead is for the starving pup alone, who, impervious to the concept of death, stays by its mother’s cold body, hoping for some food, until it begins to realize the extent of the incredible truth that faces it. 
+
+But humans are different; we mourn, not because we are hopeful for what we stand to gain, but because we are scared of what we stand to lose. We stand to lose our cherished memories, now placed within one mind rather than shared by two. We stand to lose our parents, siblings, friends, and teachers. And despite this valiant effort to somehow ‘honor the dead’ by keeping their memory alive, all we do is hurt ourselves more. 
+
+The pain of seeing a loved one die is seared into our minds for a second time as we see their lifeless body. When we realize we will never see them again every time we look at the inconspicuous clay urn, slowly gathering dust in the corner. When that same feeling makes yet another unwelcome return when we recognize that they cross our mind less and less. We claim that at some point, we find ‘peace,’ but this too is a lie. No, we find equilibrium; a middle ground between the mourning. We learn to live with this sadness. After all, we created this, and the creator always stays close to his creation. 
+
+But no other living organism acts this way – this curse is afflicted upon humans, and humans alone. Perhaps it is this same trait that gives us this fiery drive to make the heavens and ground tremble. Perhaps it is this pain which makes us inherently human. After all, Pain is a human construct.
+
+This is why. Whenever I feel emotional anguish, I remind myself: to be sad is to be alive, and to be sad is to be human. And humanity lies right there; right in that pleasant harmony between not wanting to feel pain, and appreciating the fact that we can feel it at all.
+
+Humans were destined to be sad.
+
+And we were destined to enjoy it.
+
 `,
   },
   {
     slug: 'first-week-at-vanderbilt',
-    title: 'My First Week at Vanderbilt',
+    title: 'my first week at vanderbilt',
     date: '2025-09-01',
     content: `
 Classes, new friends, and the chaos of move-in day.
